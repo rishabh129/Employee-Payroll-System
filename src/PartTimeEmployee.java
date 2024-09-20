0b746a5f3ba9b81e7ce3251266cdc79ef6ea270d
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-09-20 22:41:40 +0530

    PayrollSystem

---
+++ src/PartTimeEmployee.java
@@ -0,0 +1,17 @@
+public class PartTimeEmployee extends Employee{
+
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public PartTimeEmployee(String name, int id, int hoursWorked, double hourlyRate){
+        super(name, id);
+        this.hourlyRate=hourlyRate;
+        this.hoursWorked=hoursWorked;
+    }
+
+    @Override
+    public double CalculateSalary(){
+        return this.hoursWorked*this.hourlyRate;
+    }
+
+}
