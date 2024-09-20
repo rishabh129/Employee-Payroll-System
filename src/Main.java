d7e794548cd7252266eb627c7d3d4315ff90e63e
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-09-20 22:45:16 +0530

    create objects

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main");
+        PayrollSystem payrollSystem = new PayrollSystem();
+        FullTimeEmployee emp1 = new FullTimeEmployee("Rishabh", 1,25000);
+        PartTimeEmployee emp2 = new PartTimeEmployee("Aditya", 2, 40, 100);
+
+        payrollSystem.addEmployee(emp1);
+        payrollSystem.addEmployee(emp2);
+
+        System.out.println("Initial Employee Details:- ");
+        payrollSystem.displayEmployees();
     }
 }
