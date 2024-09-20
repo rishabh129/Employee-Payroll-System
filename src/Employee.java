7ab86124246a5e7ce1d04df901267b028f7f7325
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-09-20 21:45:50 +0530

    Employee class

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,22 @@
-public class Employee {
+abstract public class Employee {
+    private String name;
+    private int id;
+
+    public Employee(String name, int id){
+        this.name=name;
+        this.id=id;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getId(){
+        return this.id;
+    }
+
+    public abstract double CalculateSalary();
+
+    @Override
+    public String toString(){
+        return "Employee [name="+name+", id="+id+", salary="+CalculateSalary()+"]";
+    }
 }
